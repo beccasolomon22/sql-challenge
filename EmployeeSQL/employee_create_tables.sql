b14df020f8f464daa483f495bc3cbefe5c759915
beccasolomon22 <solomon.rebecca17@gmail.com>
2023-04-03 21:17:53 -0700

    new create tables with propper keys

--- EmployeeSQL/employee_create_tables.sql
+++ EmployeeSQL/employee_create_tables.sql
@@ -7,23 +7,8 @@ DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
 -- Create Tables
-CREATE TABLE departments (
-	dept_no VARCHAR,
-	dept_name VARCHAR
-);
-
-CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR
-);
-
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no INT
-);
-
 CREATE TABLE employees (
-	emp_no INT,
+	emp_no INT PRIMARY KEY,
 	emp_title_id VARCHAR,
 	birth_date VARCHAR,
 	first_name VARCHAR,
@@ -32,13 +17,37 @@ CREATE TABLE employees (
 	hire_date VARCHAR
 );
 
+CREATE TABLE departments (
+	dept_no VARCHAR,
+	dept_name VARCHAR,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
 CREATE TABLE salaries (
 	emp_no INT,
-	salary INT
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT,
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE titles (
-	title_id VARCHAR,
+	title_id VARCHAR PRIMARY KEY,
 	title VARCHAR
 );
 
